20e3855a587f4edd8890bfffae344fa3a18b0aca
Antoine Masselot <amasselot@online.net>
2019-10-09 16:22:45 +0200

    fix client component did mount

--- tp-aws-ec2/client/src/App.js
+++ tp-aws-ec2/client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import * as axios from 'axios';
 import './App.css';
@@ -10,24 +10,33 @@ const fetchMessages = async () => {
   return response.data;
 }
 
-function App() {
-  const [messages, setMessages] = useState([]);
-  fetchMessages()
-    .then(messages => setMessages(messages))
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Messages:</h1>
-        <ul>
-          {
-            messages.map(message => (
-              <li>{message.content || ''}</li>
-            ))
-          }
-        </ul>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    messages: []
+  };
+
+
+  componentDidMount() {
+    fetchMessages()
+      .then(messages => this.setState({ messages }))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Messages:</h1>
+          <ul>
+            {
+              this.state.messages.map(message => (
+                <li>{message.content || ''}</li>
+              ))
+            }
+          </ul>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
