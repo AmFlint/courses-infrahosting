df39e7709cb573d2d4e1e724b32ec3c16481e55e
Antoine Masselot <amasselot@online.net>
2019-10-17 11:36:14 +0200

    fix worker env var mySQL

--- tp-heroku/app/worker.js
+++ tp-heroku/app/worker.js
@@ -19,12 +19,7 @@ async function main() {
     const params = [message];
     try {
       const formattedQuery = mysql.format(query, params);
-      const connection = await mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
-      });
+      const connection = await mysql.createConnection(process.env.JAWSDB_URL);
     
       await connection.query(formattedQuery);
       connection.end();
